807cf23d21b187ba8f1b9a99007f87110863b15e
Pablito2442 <pmatego@gmail.com>
2026-02-19 22:54:37 +0100

    Creation & Test of @testAcquireReusableSuccess

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -18,4 +18,19 @@ class ReusablePoolTest {
         ReusablePool pool2 = ReusablePool.getInstance();
         assertSame(pool1, pool2, "getInstance debe devolver exactamente la misma instancia (Singleton)");
     }
+
+	@Test
+    @DisplayName("testAcquireReusableSuccess")
+    void testAcquireReusableSuccess() throws Exception {
+        // Modificacion del pool para comprobar que se obtienen objetos reutilizables validos
+        ReusablePool pool = ReusablePool.getInstance();
+        Reusable r = pool.acquireReusable();
+        
+        try {
+            assertNotNull(r, "Debe devolver un objeto Reusable valido");
+        } finally {
+            // Restauramos el pool a su estado original para los demas tests
+            pool.releaseReusable(r);
+        }
+    }
 }
